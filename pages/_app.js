6ea336d7b09ba8ff299de36a2632f36fdd4306b1
ManzarBilal <manzarbilal@gmail.com>
2023-07-31 16:07:00 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+export default function App({ Component, pageProps }) {
+  return(
+    <>
+    <Header />
+   <Component {...pageProps} />
+   <Footer />
+   </>
+  )
+}
