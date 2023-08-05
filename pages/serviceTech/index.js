691bb2f4c94f1481278247c9219d633a7c3439e4
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-08-05 16:56:52 +0530

    dgghd

--- pages/serviceTech/index.js
+++ pages/serviceTech/index.js
@@ -5,48 +5,52 @@ const ServiceTech = () => {
     return (
         <>
             <h2 className='text-center pt-5'>Our service techs have a heart</h2>
-            <div className='row container '>
-                <div className='marquee'>
-                    <div className='content'>
-                        <div className='mb-5 d-flex'>
-                            <div className='col-12 col-md-4 col-lg-4 mt-5 w-50'>
-                                <div className='d-flex align-items-center '>
-                                    <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={200} width={200} alt='' />
-                                    <div >
-                                        <div className='ps-5'>
-                                            <h3>Hi , I'm Raj</h3>
-                                            <h3> I'm  Certified LY3LEY</h3>
-                                            <p className='card-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                                        </div>
+            <div className='marquee'>
+                <div className='content '>
+                    <div className='row '  >
+                        <div className='col-12 col-md-4 col-lg-4 mt-5  '>
+                            <div className='row align-items-center '>
+                                <div className='col-12 col-md-4 text-md-start text-center'>
+                                <img className='rounded-circle   serviceTechImg' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" alt='' />
+                                </div>
+                                <div className='col-12 col-md-8'>
+                                    <div className=' p-3 ps-md-2 mt-md-0 mt-3 text-md-start text-center'>
+                                        <h3>Hi , I'm Raj</h3>
+                                        <h3> I'm  Certified LY3LEY</h3>
+                                        <p className='card-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className='col-12 col-md-4 col-lg-4 mt-5 w-50'>
-                                <div className='d-flex align-items-center '>
-                                    <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={200} width={200} alt='' />
-                                    <div>
-                                        <div className='ps-5'>
-                                            <h3>Hi , I'm Raj</h3>
-                                            <h3> I'm  Certified LY3LEY</h3>
-                                            <p  >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='col-12 col-md-4 col-lg-4 mt-5 w-50'>
-                                <div className='d-flex align-items-center '>
-                                    <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={200} width={200} alt='' />
-                                    <div>
-                                        <div className='ps-5'>
-                                            <h3>Hi , I'm Raj</h3>
-                                            <h3> I'm  Certified LY3LEY</h3>
-                                            <p  >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
+                        <div className='col-12 col-md-4 col-lg-4 mt-5  '>
+                            <div className='row align-items-center   '>
+                                <div className='col-12 col-md-4 text-md-start text-center'>
+                                <img className='rounded-circle   serviceTechImg' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" alt='' />
+                                </div>
+                                <div className='col-12 col-md-8'>
+                                    <div className=' p-3 ps-md-2 mt-md-0 mt-3 text-md-start text-center'>
+                                        <h3>Hi , I'm Raj</h3>
+                                        <h3> I'm  Certified LY3LEY</h3>
+                                        <p className='card-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-12 col-md-4 col-lg-4 mt-5  '>
+                            <div className='row align-items-center   '>
+                                <div className='col-12 col-md-4 text-md-start text-center'>
+                                <img className='rounded-circle   serviceTechImg' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" alt='' />
+                                </div>
+                                <div className='col-12 col-md-8'>
+                                    <div className=' p-3 ps-md-2 mt-md-0 mt-3 text-md-start text-center'>
+                                        <h3>Hi , I'm Raj</h3>
+                                        <h3> I'm  Certified LY3LEY</h3>
+                                        <p className='card-text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
