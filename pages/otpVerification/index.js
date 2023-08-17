91b914a4e52eaf602bb4be590333b4ba88c0de3d
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-08-17 11:48:03 +0530

    dhjh

--- pages/otpVerification/index.js
+++ pages/otpVerification/index.js
@@ -3,9 +3,26 @@ import OtpInput from 'react-otp-input';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import httpCommon from '@/http-common';
+import { useRouter } from 'next/router';
 const OtpVerification = () => {
     const [otp, setOtp] = useState('');
+    const [error, setError] = useState('');
 
+    const router = useRouter();
+    const handleOtp = async () => {
+        let otpData = { otp: otp }
+        try {
+            let response = await httpCommon.post("/otpPhoneVerification", otpData)
+            let { data } = response;
+            if (data?.status === true) {
+                router.push("/pricing")
+            }
+        }
+        catch (err) {
+            setError(err?.response?.data?.msg);
+        }
+    }
     return (
         <>
             <div className="text-center container">
@@ -31,7 +48,7 @@ const OtpVerification = () => {
                         >
                             <div className='mt-3 mb-3'>
                                 <Typography component="h1" variant="h5">
-                                   Otp Verification
+                                    Otp Verification
                                 </Typography>
                                 {/* <Typography color="red" component="h4" variant="h6">
                                 {error?.length > 0 ? error : ""}
@@ -40,23 +57,25 @@ const OtpVerification = () => {
                                     component="form"
                                     noValidate
                                     // onSubmit={handleSubmit}
-                                    sx={{ mt: 5 ,width:"100%"}}
+                                    sx={{ mt: 5, width: "100%" }}
                                 >
+                                    <div className='text-danger'>{error}</div>
                                     <OtpInput
-                                    inputStyle={{width:"30px"}}
+                                        inputStyle={{ width: "30px" }}
                                         value={otp}
                                         onChange={setOtp}
                                         // inputType='number'
                                         numInputs={6}
-                                        renderSeparator={<span style={{margin:"7px"}}>-</span>}
+                                        renderSeparator={<span style={{ margin: "7px" }}>-</span>}
                                         renderInput={(props) => <input   {...props} />}
                                     />
                                     <Button
-                                        type="submit"
+                                        type="button"
                                         variant="contained"
                                         sx={{ mt: 3, mb: 2 }}
+                                        onClick={handleOtp}
                                     >
-                                      Verify
+                                        Verify
                                     </Button>
                                 </Box>
                             </div>
