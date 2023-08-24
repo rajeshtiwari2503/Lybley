ebff8e2f592bd10a2914f64743748185756b4eae
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-08-24 17:57:43 +0530

    dbhhg

--- pages/faqs/index.js
+++ pages/faqs/index.js
@@ -23,11 +23,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: What is SpareTrade?</Typography>
+                <Typography>Q: What is a home warranty?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: SpareTrade is an online platform that connects customers directly with brands to purchase genuine spare parts for their appliances, electronics, and vehicles.
+                  A:  A home warranty is a service contract that covers the repair or replacement of major home systems and appliances due to normal wear and tear. It provides homeowners with peace of mind by offering financial protection against unexpected breakdowns.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -39,11 +39,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: How does SpareTrade work?</Typography>
+                <Typography>Q: What does Lybley.com offer?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: Our platform allows you to search for spare parts by brand, model, or product category. You can browse through our extensive catalog of spare parts and purchase directly from the brand with just a few clicks. We ensure that the spare parts you purchase are authentic and compatible with your product.
+                  A:  Lybley.com offers comprehensive home warranty plans that cover a wide range of appliances, systems, and major components in your home. Our plans provide protection against costly repairs and replacements.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -55,11 +55,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: Are the spare parts on SpareTrade genuine?</Typography>
+                <Typography>Q:  What is covered under Lybley.com's home warranty plans?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: Yes, we work closely with brands to source only the highest quality and most reliable spare parts that are compatible with your product.
+                  A:  Our home warranty plans cover items such as HVAC systems, electrical systems, plumbing, kitchen appliances, water heaters, and more. For a detailed list of coverage, please refer to our website or plan documentation.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -71,11 +71,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: What products do SpareTrade offer spare parts for?</Typography>
+                <Typography>Q: How do I submit a service request?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: We offer spare parts for a wide range of products, including refrigerators, air conditioners, washing machines, televisions, mobile phones, laptops, and vehicles, among others.
+                  A: You can easily submit a service request through our website or mobile app. Log in to your account, provide details about the issue, and our team will promptly dispatch a qualified technician to assist you.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -87,11 +87,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: How do I pay for my spare parts on SpareTrade?</Typography>
+                <Typography>Q: Are there any limitations or exclusions to the coverage?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: We offer secure payment options, including credit card, debit card, and online banking.
+                  A:  While our plans cover a wide range of items, there may be certain limitations and exclusions. These can vary based on the plan you choose. It's important to review the plan details and terms to understand coverage specifics.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -103,11 +103,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: How long will it take to receive my spare parts?</Typography>
+                <Typography>Q: How quickly will my service request be addressed?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Delivery times may vary depending on your location and the availability of the spare parts. We work with reliable delivery partners to ensure that you receive your spare parts as quickly as possible.
+                We pride ourselves on delivering prompt service. Typically, service requests are addressed within [timeframe]. Urgent issues are prioritized, ensuring you receive timely assistance.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -119,11 +119,11 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: What is the return policy for spare parts purchased on SpareTrade?</Typography>
+                <Typography>Q: Do I need to pay a service fee when a technician visits?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: We have a hassle-free return policy that allows you to return the spare parts within a specified period if they are not compatible with your product or are found to be defective.
+                  A:  Yes, a service fee is applicable for each service request. This fee helps cover the cost of dispatching qualified technicians to your home. The fee amount varies based on your plan and location.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -135,15 +135,81 @@ const Faqs = () => {
                 aria-controls="panel1a-content"
                 id="panel1a-header"
               >
-                <Typography>Q: How do I contact customer support at SpareTrade?</Typography>
+                <Typography>Q: Can I choose my own repair technician?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  A: Our dedicated customer support team is always available to assist you with any queries or concerns you may have. You can reach us via email or phone, and we will be happy to help.
+                  A: While we have a network of qualified technicians, you can request a specific technician if you have a preferred service provider. However, we encourage you to use our network for reliable and efficient service.
                 </Typography>
               </AccordionDetails>
             </Accordion>
           </div>
+          <div className='mt-5'>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Q: How do I renew my home warranty plan?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  A: Renewing your home warranty plan is easy. You will receive renewal information prior to the expiration date. Simply follow the instructions provided to continue enjoying uninterrupted coverage.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className='mt-5'>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Q:  What if I want to cancel my home warranty plan?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  A:  If you wish to cancel your plan, please contact our customer support team. Keep in mind that cancellation terms may vary based on the plan you've chosen.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className='mt-5'>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Q:  Do you offer plans for real estate professionals and property managers?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  A:  Yes, we offer specialized plans for real estate professionals and property managers. These plans provide added value and protection for your clients or tenants.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          <div className='mt-5'>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Q: How do I contact Lybley.com's customer support?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  A:  You can reach our customer support team through phone, email, or live chat on our website. We're here to assist you with any inquiries or concerns.
+                  For more detailed information about our home warranty plans, coverage, and services, please visit our website or contact our customer support team.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          
         </div>
       </div>
     
