d33f79d8cf2aaf773c98762df8a87b3cc8e27e94
ManzarBilal <manzarbilal@gmail.com>
2023-10-03 18:25:13 +0530

    Fds

--- pages/subscription/index.js
+++ pages/subscription/index.js
@@ -8,64 +8,64 @@ const Subscription = () => {
                 <div className='col-12 col-md-3 col-lg-3 p-2 '>
                     <div className='shadow p-3 commonTransition'>
                     <div className='d-flex align-items-center '>
-                        <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={60} width={60} alt='' />
+                        <img className='rounded-circle' src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" height={60} width={60} alt='' />
                         <div >
                             <div className='ps-3'>
                                 <div className='fw-bold'>Mukesh</div>
-                                <div> I'm  Certified LY3LEY</div>
+                                {/* <div> I'm  Certified LYBLEY</div> */}
                             </div>
                         </div>
                     </div>
                     <div className='mt-3 '>
-                    Whether you're requesting service through the App or on the website, we'll ask how we can help, where you need us, and when you need us. Then we dispatch a Super Certified professional, and you're on your way to easy living again.
+                    Requesting through App or website, we inquire about your needs, location, and timing. Lybley Certified professional dispatched, ensuring your ease.
                     </div>
                     </div>
                 </div>
                 <div className='col-12 col-md-3 col-lg-3 p-2 '>
                     <div className='shadow p-3 commonTransition'>
                     <div className='d-flex align-items-center '>
-                        <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={60} width={60} alt='' />
+                        <img className='rounded-circle' src="https://www.shutterstock.com/shutterstock/photos/2069457431/display_1500/stock-photo-handsome-smiling-young-man-with-folded-arms-beautiful-portrait-of-laughing-joyful-cheerful-men-2069457431.jpg" height={60} width={60} alt='' />
                         <div >
                             <div className='ps-3'>
-                                <div className='fw-bold'>Mukesh</div>
-                                <div> I'm  Certified LY3LEY</div>
+                                <div className='fw-bold'>Rahul</div>
+                                {/* <div> I'm  Certified LYBLEY</div> */}
                             </div>
                         </div>
                     </div>
                     <div className='mt-3 '>
-                    Whether you're requesting service through the App or on the website, we'll ask how we can help, where you need us, and when you need us. Then we dispatch a Super Certified professional, and you're on your way to easy living again.
+                    Whether via the App or website, we inquire about your needs, location, and timing. We then send a Lybley Certified expert, restoring your comfort.
                     </div>
                     </div>
                 </div>
                 <div className='col-12 col-md-3 col-lg-3 p-2 '>
                     <div className='shadow p-3 commonTransition'>
                     <div className='d-flex align-items-center '>
-                        <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={60} width={60} alt='' />
+                        <img className='rounded-circle' src="https://img.freepik.com/premium-photo/indian-young-happy-man-image_181020-2462.jpg" height={60} width={60} alt='' />
                         <div >
                             <div className='ps-3'>
-                                <div className='fw-bold'>Mukesh</div>
-                                <div> I'm  Certified LY3LEY</div>
+                                <div className='fw-bold'>Sachin</div>
+                                {/* <div> I'm  Certified LY3LEY</div> */}
                             </div>
                         </div>
                     </div>
                     <div className='mt-3 '>
-                    Whether you're requesting service through the App or on the website, we'll ask how we can help, where you need us, and when you need us. Then we dispatch a Super Certified professional, and you're on your way to easy living again.
+                    Requesting via the App or website, we seek your assistance details, location, and timing. A Lybley Certified expert is dispatched, restoring ease in your life.
                     </div>
                     </div>
                 </div>
                 <div className='col-12 col-md-3 col-lg-3 p-2 '>
                     <div className='shadow p-3 commonTransition'>
                     <div className='d-flex align-items-center '>
-                        <img className='rounded-circle' src="https://hairstylecamp.com/wp-content/uploads/straight-hairstyle-for-men-with-glasses.jpg.webp" height={60} width={60} alt='' />
+                        <img className='rounded-circle' src="https://media.istockphoto.com/id/664572810/photo/portrait-of-a-young-man.webp?b=1&s=170667a&w=0&k=20&c=lO0r0iOZjrqYoRzFaoIMvqJr4zS40Iy6hKYYyMQmc_g=" height={60} width={60} alt='' />
                         <div >
                             <div className='ps-3'>
-                                <div className='fw-bold'>Mukesh</div>
-                                <div> I'm  Certified LY3LEY</div>
+                                <div className='fw-bold'>Vivek</div>
+                                {/* <div> I'm  Certified LY3LEY</div> */}
                             </div>
                         </div>
                     </div>
                     <div className='mt-3 '>
-                    Whether you're requesting service through the App or on the website, we'll ask how we can help, where you need us, and when you need us. Then we dispatch a Super Certified professional, and you're on your way to easy living again.
+                    When seeking assistance on our App or website, we inquire about your needs, location, and preferred time. Next, we send a Lybley Certified expert, restoring your convenience.
                     </div>
                     </div>
                 </div>
